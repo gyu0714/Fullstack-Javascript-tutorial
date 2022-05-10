9e9bb938b580fe65559fad741c304e5eee8a57c3
gyu0714 <lagyu99@gmail.com>
2022-05-10 18:25:17 +0900

    [UPDATE] step01_basic

---
+++ step01_basic/js/index.js
@@ -0,0 +1,29 @@
+// 2. syntax
+// let var1 = 1;
+// var var2 = 'data';
+
+// 단일 주석
+/* 다중 주석 */
+
+// 변수명 생성 : 영문자(대소문자), 숫자, _, $
+// let varTest = '';
+// let var2 = '';
+
+// 3. script
+// console.log('Hello, JS(외부)');
+let var1 = "출력";
+// 4-1.console.log
+console.log(var1);
+
+// 4-2. prompt
+// let var2 = prompt();
+// console.log(var2);
+
+// 4-3. alert
+// alert(var1);
+
+// 4-4. document.write()
+// document.write(var1);
+
+// 4-5 HTML요소를 통한 출력
+innerHTML;
